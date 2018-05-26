4b28a9bf69e9bee25e7a9deb82a1485bea8de383
Ahmed <a.abdelaziz44@gmail.com>
2018-05-26 15:04:56 +0200

    Creating a reactive form in code while syncing the html with it.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  genders = ['male', 'female'];
+  signUpForm: FormGroup;
+
   suggestUserName() {
-    const suggestedName = 'Superuser'
+    const suggestedName = 'Superuser';
+  }
+
+  ngOnInit() {
+    this.signUpForm = new FormGroup({
+      'username': new FormControl(null),
+      'email': new FormControl(null),
+      'gender': new FormControl('male')
+    });
   }
 }
